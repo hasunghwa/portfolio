cb2d32106419c270cf20065ccc70e4bdcb66128e
hasunghwa <tjdghk3920@naver.com>
2023-03-03 15:20:15 +0900

    📚 Docs : 문구 수정

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -20,7 +20,7 @@ function Home() {
 
               <h1 className="heading-name">
                 신입
-                <strong className="main-name"> 프론트엔드 개발자</strong>
+                <strong className="main-name"> 웹 개발자</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -3,12 +3,6 @@ import { Container, Row, Col } from "react-bootstrap";
 
 import myImg from "../../Assets/tteok.png";
 import Tilt from "react-parallax-tilt";
-import {
-  AiFillGithub,
-  AiOutlineTwitter,
-  AiFillInstagram,
-} from "react-icons/ai";
-import { FaLinkedinIn } from "react-icons/fa";
 
 function Home2() {
   return (
@@ -20,23 +14,12 @@ function Home2() {
               <span className="green"> ABOUT </span> ME
             </h1>
             <p className="home-about-body">
-              옛날 속담에
               <i>
-                <b className="green"> "보기 좋은 떡이 먹기도 좋다" </b>
-              </i>
-              라는 말이 있습니다.
+                <b className="green"> "백발코더를 꿈꿉니다." </b>
+              </i> <br />
               <br />
-              <br />
-              웹프로그래밍을 떡으로 비유하자면 떡을 보기 좋게 꾸미는 것이
-              <br />
-              프론트엔드 개발자의 일이라고 생각합니다.
-              <br />
-              <br />
-              예쁜 떡을 보고 있으면 먹고 싶은 생각이 들듯이
-              <br /> 보기만해도 사용하고싶은 생각이 들게 만드는 UI를
-              <br />
-              만드는 프론트엔드 개발자가 되고싶습니다.
-              <br />
+              지속성을 가진 개발자가 되기 위해서 해야 할 것들이 무엇인지 고민하고, 
+              <br /> 고민에서 실천으로 옮기자!
               <br />
             </p>
           </Col>
@@ -46,56 +29,6 @@ function Home2() {
             </Tilt>
           </Col>
         </Row>
-        {/* <Row>
-          <Col md={12} className="home-about-social">
-            <h1>FIND ME ON</h1>
-            <p>
-              Feel free to <span className="purple">connect </span>with me
-            </p>
-            <ul className="home-about-social-links">
-              <li className="social-icons">
-                <a
-                  href="https://github.com/soumyajit4419"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <AiFillGithub />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://twitter.com/Soumyajit4419"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <AiOutlineTwitter />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://www.linkedin.com/in/soumyajit4419/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <FaLinkedinIn />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://www.instagram.com/s.o.u.m.y.a_j.i.t/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour home-social-icons"
-                >
-                  <AiFillInstagram />
-                </a>
-              </li>
-            </ul>
-          </Col>
-        </Row> */}
       </Container>
     </Container>
   );
